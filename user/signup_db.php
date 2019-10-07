29e802106d0ec095c62fe7ad9cb6affdf5574068
mukul sharma <mukulsharma1323@gmail.com>
2019-10-08 05:06:50 +0530

    added add product page, vendor and user

---
+++ user/signup_db.php
@@ -0,0 +1,29 @@
+<?php
+// configure the database 
+
+include('db_config.php');
+
+// receive values from HTML FORM as POST method 
+if(isset($_POST['email']) && isset($_POST['password']))
+{
+  $Email=$_POST['email'];
+  $Password=$_POST['password'];
+  $Shop_name=$_POST['shop_name'];
+// echo values to test the variables, if they have value or not.
+// echo "$Shop_name";
+// echo "$Password";
+// echo "$Email";
+// insert values into database
+
+ $query="INSERT INTO vendor (Email,Password,Shop_Name)Values('$Email','$Password','$Shop_name')";
+  $result = mysqli_query($connection, $query);
+  if($result)
+  {
+    header("location: index.php");
+  }
+  else
+  {
+    echo "user Registration failed";
+  }
+}
+?>
