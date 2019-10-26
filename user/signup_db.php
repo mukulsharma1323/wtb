935eeff5ebb43f5263c42cf9e3bc1eeb7ff954b6
AmitDas01 <48611114+AmitDas01@users.noreply.github.com>
2019-10-26 05:15:42 -0700

    Update signup_db.php

--- user/signup_db.php
+++ user/signup_db.php
@@ -1,29 +1,49 @@
 <?php
-// configure the database 
 
-include('db_config.php');
+if(isset($_POST['submit'])){
 
-// receive values from HTML FORM as POST method 
-if(isset($_POST['email']) && isset($_POST['password']))
-{
-  $Email=$_POST['email'];
-  $Password=$_POST['password'];
-  $Shop_name=$_POST['shop_name'];
-// echo values to test the variables, if they have value or not.
-// echo "$Shop_name";
-// echo "$Password";
-// echo "$Email";
-// insert values into database
+    include('db_config.php');
+ 
+    $email = mysqli_real_escape_string($connection, $_POST['email']);  
+    $password = mysqli_real_escape_string($connection, $_POST['password']);
+    $shop_name = mysqli_real_escape_string($connection, $_POST['shop_name']);
 
- $query="INSERT INTO vendor (Email,Password,Shop_Name)Values('$Email','$Password','$Shop_name')";
-  $result = mysqli_query($connection, $query);
-  if($result)
-  {
-    header("location: index.php");
+    //Error handlers
+    if(empty($email) || empty($password) || empty($shop_name)){
+      header("Location: signup.php?signup=empty");
+      exit();   
+    }else{
+      //Check if input valid
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+          header("Location: signup.php?signup=invalidemail");
+          exit();       
+        }else{
+            if(!preg_match("/^[a-zA-Z]*$/", $shop_name)){
+              header("Location: signup.php?signup=invalid");
+              exit();     
+            }else{
+              $sql="SELECT * FROM users_details WHERE Shop_Name='$shop_name'";
+              $result=mysqli_query($connection, $sql);
+              $resultcheck = mysqli_num_rows($result);
+              if($resultcheck > 0){
+                header("Location: signup.php?signup=usertaken");
+                exit();
+              }
+              else{
+                //Hashing the password
+                $hashedpwd=password_hash($password, PASSWORD_DEFAULT);
+                //User Entry to the Database
+                $sql = "INSERT INTO users_details (Email,Password,Shop_Name) VALUES ('$email','$hashedpwd','$shop_name');" ;
+                mysqli_query($connection, $sql);
+                header("Location: index.php?signup=success");
+                exit(); 
+              }
+            }
+        }
   }
-  else
-  {
-    echo "user Registration failed";
-  }
+
+}else{
+  header("Location: signup.php");
+  exit();
 }
 ?>
