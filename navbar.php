4da7e7bc5a73ca8f314743f6a08d1338b7b50ed1
shashwatsr2712 <shashwatsr2712@gmail.com>
2019-12-30 10:38:44 +0530

    Add form submit attributes for search form
    ID given to the search form and href attribute added to anchor tag for submitting it

--- navbar.php
+++ navbar.php
@@ -1,86 +1,85 @@
-<header class="header-style-1">
-
+<header class="header-style-1"> 
+  
   <!-- ============================================== TOP MENU ============================================== -->
   <div class="top-bar animate-dropdown">
     <div class="container">
       <div class="header-top-inner">
-        <!-- <div class="cnt-account">
+        <div class="cnt-account">
           <ul class="list-unstyled">
             <li><a href="#"><i class="icon fa fa-user"></i>My Account</a></li>
             <li><a href="#"><i class="icon fa fa-heart"></i>Wishlist</a></li>
             <li><a href="#"><i class="icon fa fa-lock"></i>Login</a></li>
           </ul>
-        </div> -->
-
+        </div>
         <!-- /.cnt-account -->
-
-
+        
+        
         <!-- /.cnt-cart -->
         <div class="clearfix"></div>
       </div>
-      <!-- /.header-top-inner -->
+      <!-- /.header-top-inner --> 
     </div>
-    <!-- /.container -->
+    <!-- /.container --> 
   </div>
-  <!-- /.header-top -->
+  <!-- /.header-top --> 
   <!-- ============================================== TOP MENU : END ============================================== -->
 
 
   <div class="main-header">
     <div class="container">
       <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
+        <div class="col-xs-12 col-sm-12 col-md-3 logo-holder"> 
           <!-- ============================================================= LOGO ============================================================= -->
-          <div class="logo"> <a href="index.php">
+          <div class="logo"> <a href="home.html">
            <!-- <img src="assets/images/logo.png" alt="logo">  -->
            <h2 style="color:white; margin-top: 0px">Where Is To Buy</h2>
          </a> </div>
-          <!-- /.logo -->
+          <!-- /.logo --> 
           <!-- ============================================================= LOGO : END ============================================================= --> </div>
         <!-- /.logo-holder -->
-
-        <div class="col-xs-12 col-sm-12 col-md-7 top-search-holder">
-          <!-- /.contact-row -->
+        
+        <div class="col-xs-12 col-sm-12 col-md-7 top-search-holder"> 
+          <!-- /.contact-row --> 
           <!-- ============================================================= SEARCH AREA ============================================================= -->
           <div class="search-area">
-            <form>
+            <form id="product-search-form" method="GET" action="search.php">
               <div class="control-group">
                 <ul class="categories-filter animate-dropdown">
-                  <li class="dropdown"> <a class="dropdown-toggle"  data-toggle="dropdown" href="category.php">Cities <b class="caret"></b></a>
+                  <li class="dropdown"> <a class="dropdown-toggle"  data-toggle="dropdown" href="category.html">Categories <b class="caret"></b></a>
                     <ul class="dropdown-menu" role="menu" >
-                      <!-- <li class="menu-header"></li> -->
-                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.php">Meerut</a></li>
-                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.php">- Noida</a></li>
-                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.php">- More Coming Soon</a></li>
+                      <li class="menu-header">Computer</li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Clothing</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Electronics</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Shoes</a></li>
+                      <li role="presentation"><a role="menuitem" tabindex="-1" href="category.html">- Watches</a></li>
                     </ul>
                   </li>
                 </ul>
                 <input class="search-field" placeholder="Search here..." />
-                <a class="search-button" href="#" ></a> </div>
+                <a class="search-button" href="javascript:$('#product-search-form').submit();" ></a> </div>
             </form>
           </div>
-          <!-- /.search-area -->
-          <!-- ============================================================= SEARCH AREA : END ============================================================= --> 
-        </div>
+          <!-- /.search-area --> 
+          <!-- ============================================================= SEARCH AREA : END ============================================================= --> </div>
         <!-- /.top-search-holder -->
         
-
-        <!-- /.top-cart-row -->
+        
+        <!-- /.top-cart-row --> 
       </div>
-      <!-- /.row -->
-
+      <!-- /.row --> 
+      
     </div>
-    <!-- /.container -->
-
+    <!-- /.container --> 
+    
   </div>
-  <!-- /.main-header -->
-
+  <!-- /.main-header --> 
+  
   <!-- ============================================== NAVBAR ============================================== -->
   <div class="header-nav animate-dropdown">
     <div class="container">
       <div class="yamm navbar navbar-default" role="navigation">
         <div class="navbar-header">
-       <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+       <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> 
        <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
         </div>
         <div class="nav-bg-class">
@@ -88,25 +87,103 @@
             <div class="nav-outer">
               <ul class="nav navbar-nav">
                 <li class="active dropdown yamm-fw"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Home</a> </li>
+                <li class="dropdown mega-menu"> 
+                <a href="category.html"  data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Electronics <span class="menu-label hot-menu hidden-xs">new</span> </a>
+                  <ul class="dropdown-menu container">
+                    <li>
+                      <div class="yamm-content">
+                        <div class="row">
+                          <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
+                            <h2 class="title">Laptops</h2>
+                            <ul class="links">
+                              <li><a href="#">Gaming</a></li>
+                              <li><a href="#">Laptop Skins</a></li>
+                              <li><a href="#">Apple</a></li>
+                              <li><a href="#">Dell</a></li>
+                              <li><a href="#">Lenovo</a></li>
+                              <li><a href="#">Microsoft</a></li>
+                              <li><a href="#">Asus</a></li>
+                              <li><a href="#">Adapters</a></li>
+                              <li><a href="#">Batteries</a></li>
+                              <li><a href="#">Cooling Pads</a></li>
+                            </ul>
+                          </div>
+                          <!-- /.col -->
+                          
+                          <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
+                            <h2 class="title">Desktops</h2>
+                            <ul class="links">
+                              <li><a href="#">Routers & Modems</a></li>
+                              <li><a href="#">CPUs, Processors</a></li>
+                              <li><a href="#">PC Gaming Store</a></li>
+                              <li><a href="#">Graphics Cards</a></li>
+                              <li><a href="#">Components</a></li>
+                              <li><a href="#">Webcam</a></li>
+                              <li><a href="#">Memory (RAM)</a></li>
+                              <li><a href="#">Motherboards</a></li>
+                              <li><a href="#">Keyboards</a></li>
+                              <li><a href="#">Headphones</a></li>
+                            </ul>
+                          </div>
+                          <!-- /.col -->
+                          
+                          <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
+                            <h2 class="title">Cameras</h2>
+                            <ul class="links">
+                              <li><a href="#">Accessories</a></li>
+                              <li><a href="#">Binoculars</a></li>
+                              <li><a href="#">Telescopes</a></li>
+                              <li><a href="#">Camcorders</a></li>
+                              <li><a href="#">Digital</a></li>
+                              <li><a href="#">Film Cameras</a></li>
+                              <li><a href="#">Flashes</a></li>
+                              <li><a href="#">Lenses</a></li>
+                              <li><a href="#">Surveillance</a></li>
+                              <li><a href="#">Tripods</a></li>
+                            </ul>
+                          </div>
+                          <!-- /.col -->
+                          <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
+                            <h2 class="title">Mobile Phones</h2>
+                            <ul class="links">
+                              <li><a href="#">Apple</a></li>
+                              <li><a href="#">Samsung</a></li>
+                              <li><a href="#">Lenovo</a></li>
+                              <li><a href="#">Motorola</a></li>
+                              <li><a href="#">LeEco</a></li>
+                              <li><a href="#">Asus</a></li>
+                              <li><a href="#">Acer</a></li>
+                              <li><a href="#">Accessories</a></li>
+                              <li><a href="#">Headphones</a></li>
+                              <li><a href="#">Memory Cards</a></li>
+                            </ul>
+                          </div>
+                          <div class="col-xs-12 col-sm-12 col-md-4 col-menu custom-banner"> <a href="#"><img alt="" src="assets/images/banners/banner-side.png"></a> </div>
+                        </div>
+                        <!-- /.row --> 
+                      </div>
+                      <!-- /.yamm-content --> </li>
+                  </ul>
+                </li>
                 
                 <li class="dropdown  navbar-right special-menu"> <a href="#">Todays offer</a> </li>
               </ul>
               <!-- /.navbar-nav -->
               <div class="clearfix"></div>
             </div>
-            <!-- /.nav-outer -->
+            <!-- /.nav-outer --> 
           </div>
-          <!-- /.navbar-collapse -->
-
+          <!-- /.navbar-collapse --> 
+          
         </div>
-        <!-- /.nav-bg-class -->
+        <!-- /.nav-bg-class --> 
       </div>
-      <!-- /.navbar-default -->
+      <!-- /.navbar-default --> 
     </div>
-    <!-- /.container-class -->
-
+    <!-- /.container-class --> 
+    
   </div>
-  <!-- /.header-nav -->
-  <!-- ============================================== NAVBAR : END ============================================== -->
-
+  <!-- /.header-nav --> 
+  <!-- ============================================== NAVBAR : END ============================================== --> 
+  
 </header>
