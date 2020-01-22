3a785ac269b8251dab84accfe0f609eabc54a0dd
shashwatsr2712 <shashwatsr2712@gmail.com>
2020-01-22 20:28:20 +0530

    Update file for AJAX live search feature

--- navbar.php
+++ navbar.php
@@ -43,7 +43,7 @@
           <!-- ============================================================= SEARCH AREA ============================================================= -->
           <div class="search-area">
             <form id="product-search-form" method="GET" action="search.php">
-              <div class="control-group">
+              <div class="control-group search-field-div">
                 <ul class="categories-filter animate-dropdown">
                   <li class="dropdown"> <a class="dropdown-toggle"  data-toggle="dropdown" href="category.html">Categories <b class="caret"></b></a>
                     <ul class="dropdown-menu" role="menu" >
@@ -55,8 +55,10 @@
                     </ul>
                   </li>
                 </ul>
-                <input class="search-field" placeholder="Search here..." />
-                <a class="search-button" href="javascript:$('#product-search-form').submit();" ></a> </div>
+                <input type="text" autocomplete="off" class="search-field" id="search-box" name="product_query" placeholder="Search here..."/>
+                <a class="search-button" href="javascript:$('#product-search-form').submit();" ></a> 
+                <div class="results-live" id="result-live"></div>
+              </div>  
             </form>
           </div>
           <!-- /.search-area --> 
@@ -78,112 +80,117 @@
   <div class="header-nav animate-dropdown">
     <div class="container">
       <div class="yamm navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-       <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> 
-       <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-        </div>
-        <div class="nav-bg-class">
-          <div class="navbar-collapse collapse" id="mc-horizontal-menu-collapse">
-            <div class="nav-outer">
-              <ul class="nav navbar-nav">
-                <li class="active dropdown yamm-fw"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Home</a> </li>
-                <li class="dropdown mega-menu"> 
-                <a href="category.html"  data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Electronics <span class="menu-label hot-menu hidden-xs">new</span> </a>
-                  <ul class="dropdown-menu container">
-                    <li>
-                      <div class="yamm-content">
-                        <div class="row">
-                          <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
-                            <h2 class="title">Laptops</h2>
-                            <ul class="links">
-                              <li><a href="#">Gaming</a></li>
-                              <li><a href="#">Laptop Skins</a></li>
-                              <li><a href="#">Apple</a></li>
-                              <li><a href="#">Dell</a></li>
-                              <li><a href="#">Lenovo</a></li>
-                              <li><a href="#">Microsoft</a></li>
-                              <li><a href="#">Asus</a></li>
-                              <li><a href="#">Adapters</a></li>
-                              <li><a href="#">Batteries</a></li>
-                              <li><a href="#">Cooling Pads</a></li>
-                            </ul>
-                          </div>
-                          <!-- /.col -->
-                          
-                          <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
-                            <h2 class="title">Desktops</h2>
-                            <ul class="links">
-                              <li><a href="#">Routers & Modems</a></li>
-                              <li><a href="#">CPUs, Processors</a></li>
-                              <li><a href="#">PC Gaming Store</a></li>
-                              <li><a href="#">Graphics Cards</a></li>
-                              <li><a href="#">Components</a></li>
-                              <li><a href="#">Webcam</a></li>
-                              <li><a href="#">Memory (RAM)</a></li>
-                              <li><a href="#">Motherboards</a></li>
-                              <li><a href="#">Keyboards</a></li>
-                              <li><a href="#">Headphones</a></li>
-                            </ul>
-                          </div>
-                          <!-- /.col -->
-                          
-                          <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
-                            <h2 class="title">Cameras</h2>
-                            <ul class="links">
-                              <li><a href="#">Accessories</a></li>
-                              <li><a href="#">Binoculars</a></li>
-                              <li><a href="#">Telescopes</a></li>
-                              <li><a href="#">Camcorders</a></li>
-                              <li><a href="#">Digital</a></li>
-                              <li><a href="#">Film Cameras</a></li>
-                              <li><a href="#">Flashes</a></li>
-                              <li><a href="#">Lenses</a></li>
-                              <li><a href="#">Surveillance</a></li>
-                              <li><a href="#">Tripods</a></li>
-                            </ul>
-                          </div>
-                          <!-- /.col -->
-                          <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
-                            <h2 class="title">Mobile Phones</h2>
-                            <ul class="links">
-                              <li><a href="#">Apple</a></li>
-                              <li><a href="#">Samsung</a></li>
-                              <li><a href="#">Lenovo</a></li>
-                              <li><a href="#">Motorola</a></li>
-                              <li><a href="#">LeEco</a></li>
-                              <li><a href="#">Asus</a></li>
-                              <li><a href="#">Acer</a></li>
-                              <li><a href="#">Accessories</a></li>
-                              <li><a href="#">Headphones</a></li>
-                              <li><a href="#">Memory Cards</a></li>
-                            </ul>
-                          </div>
-                          <div class="col-xs-12 col-sm-12 col-md-4 col-menu custom-banner"> <a href="#"><img alt="" src="assets/images/banners/banner-side.png"></a> </div>
-                        </div>
-                        <!-- /.row --> 
-                      </div>
-                      <!-- /.yamm-content --> </li>
-                  </ul>
-                </li>
-                
-                <li class="dropdown  navbar-right special-menu"> <a href="#">Todays offer</a> </li>
-              </ul>
-              <!-- /.navbar-nav -->
-              <div class="clearfix"></div>
+          <div class="navbar-header">
+              <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> 
+              <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+               </div>
+               <div class="nav-bg-class">
+                 <div class="navbar-collapse collapse" id="mc-horizontal-menu-collapse">
+                   <div class="nav-outer">
+                     <ul class="nav navbar-nav">
+       <<<<<<< HEAD
+                       <li class="active dropdown yamm-fw"> <a href="index.php">Home</a> </li>
+                       <li class="active dropdown yamm-fw"> <a href="vendors.php">Sellers</a> </li>
+       =======
+                       <li class="active dropdown yamm-fw"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Home</a> </li>
+                       <li class="dropdown mega-menu"> 
+                       <a href="category.html"  data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">Electronics <span class="menu-label hot-menu hidden-xs">new</span> </a>
+                         <ul class="dropdown-menu container">
+                           <li>
+                             <div class="yamm-content">
+                               <div class="row">
+                                 <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
+                                   <h2 class="title">Laptops</h2>
+                                   <ul class="links">
+                                     <li><a href="#">Gaming</a></li>
+                                     <li><a href="#">Laptop Skins</a></li>
+                                     <li><a href="#">Apple</a></li>
+                                     <li><a href="#">Dell</a></li>
+                                     <li><a href="#">Lenovo</a></li>
+                                     <li><a href="#">Microsoft</a></li>
+                                     <li><a href="#">Asus</a></li>
+                                     <li><a href="#">Adapters</a></li>
+                                     <li><a href="#">Batteries</a></li>
+                                     <li><a href="#">Cooling Pads</a></li>
+                                    </ul>
+                                  </div>
+                                  <!-- /.col -->
+                                  
+                                  <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
+                                    <h2 class="title">Desktops</h2>
+                                    <ul class="links">
+                                      <li><a href="#">Routers & Modems</a></li>
+                                      <li><a href="#">CPUs, Processors</a></li>
+                                      <li><a href="#">PC Gaming Store</a></li>
+                                      <li><a href="#">Graphics Cards</a></li>
+                                      <li><a href="#">Components</a></li>
+                                      <li><a href="#">Webcam</a></li>
+                                      <li><a href="#">Memory (RAM)</a></li>
+                                      <li><a href="#">Motherboards</a></li>
+                                      <li><a href="#">Keyboards</a></li>
+                                      <li><a href="#">Headphones</a></li>
+                                    </ul>
+                                  </div>
+                                  <!-- /.col -->
+                                  
+                                  <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
+                                    <h2 class="title">Cameras</h2>
+                                    <ul class="links">
+                                      <li><a href="#">Accessories</a></li>
+                                      <li><a href="#">Binoculars</a></li>
+                                      <li><a href="#">Telescopes</a></li>
+                                      <li><a href="#">Camcorders</a></li>
+                                      <li><a href="#">Digital</a></li>
+                                      <li><a href="#">Film Cameras</a></li>
+                                      <li><a href="#">Flashes</a></li>
+                                      <li><a href="#">Lenses</a></li>
+                                      <li><a href="#">Surveillance</a></li>
+                                      <li><a href="#">Tripods</a></li>
+                                    </ul>
+                                  </div>
+                                  <!-- /.col -->
+                                  <div class="col-xs-12 col-sm-12 col-md-2 col-menu">
+                                    <h2 class="title">Mobile Phones</h2>
+                                    <ul class="links">
+                                      <li><a href="#">Apple</a></li>
+                                      <li><a href="#">Samsung</a></li>
+                                      <li><a href="#">Lenovo</a></li>
+                                      <li><a href="#">Motorola</a></li>
+                                      <li><a href="#">LeEco</a></li>
+                                      <li><a href="#">Asus</a></li>
+                                      <li><a href="#">Acer</a></li>
+                                      <li><a href="#">Accessories</a></li>
+                                      <li><a href="#">Headphones</a></li>
+                                      <li><a href="#">Memory Cards</a></li>
+                                    </ul>
+                                  </div>
+                                  <div class="col-xs-12 col-sm-12 col-md-4 col-menu custom-banner"> <a href="#"><img alt="" src="assets/images/banners/banner-side.png"></a> </div>
+                                </div>
+                                <!-- /.row --> 
+                              </div>
+                              <!-- /.yamm-content --> </li>
+                          </ul>
+                        </li>
+                        
+        >>>>>>> 7899f9ffcbb68f3737d7502605555d02bafc0cd8
+                        <li class="dropdown  navbar-right special-menu"> <a href="#">Todays offer</a> </li>
+                      </ul>
+                      <!-- /.navbar-nav -->
+                      <div class="clearfix"></div>
+                    </div>
+                    <!-- /.nav-outer --> 
+                  </div>
+                  <!-- /.navbar-collapse --> 
+                  
+                </div>
+                <!-- /.nav-bg-class --> 
+              </div>
+              <!-- /.navbar-default --> 
             </div>
-            <!-- /.nav-outer --> 
+            <!-- /.container-class --> 
+            
           </div>
-          <!-- /.navbar-collapse --> 
+          <!-- /.header-nav --> 
+          <!-- ============================================== NAVBAR : END ============================================== --> 
           
-        </div>
-        <!-- /.nav-bg-class --> 
-      </div>
-      <!-- /.navbar-default --> 
-    </div>
-    <!-- /.container-class --> 
-    
-  </div>
-  <!-- /.header-nav --> 
-  <!-- ============================================== NAVBAR : END ============================================== --> 
-  
-</header>
+        </header>
